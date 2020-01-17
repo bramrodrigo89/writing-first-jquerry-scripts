2bc4161554a72962e30adc4b8129b59302281daf
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-01-17 15:33:35 +0000

    first time using jquery to change styles in html

---
+++ js/my_script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("h2").addClass("underline") //underlines all <h2> elements
+    $("ul").addClass("border") //creates border around ul elements
+
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
+})
